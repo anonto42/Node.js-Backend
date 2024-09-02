8d0eeb288bc71f3bb5d3b0e301e17c46d764c2c3
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-02 09:00:16 +0600

    route adding

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import { userRegister } from '../controller/user.controller.js';
+import { userRegister , postProduct } from '../controller/user.controller.js';
 
 const createRoute = Router();
 
 createRoute.route("/register").post(userRegister);
+
+createRoute.route("/postProduct").post(postProduct);
 
 
 export default createRoute;
