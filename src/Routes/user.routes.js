31fc6268f7a894db4b01005097882ba4813f27d5
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-02 11:10:22 +0600

    project upload updated

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import { userRegister , postProduct } from '../controller/user.controller.js';
+import { upload } from '../middleware/uploader.middleware.js';
 
 const createRoute = Router();
 
 createRoute.route("/register").post(userRegister);
 
-createRoute.route("/postProduct").post(postProduct);
+createRoute.route("/postProduct").post( upload.fields([{name:"image1",maxCount:1},{name:"image2",maxCount:1},{name:"image3",maxCount: 1}]) , postProduct);
 
 
 export default createRoute;
