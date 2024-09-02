0232a7c453ab8dc73fa38fbf4c1e6cd2782879ab
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-02 08:57:47 +0600

    post added

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,30 @@
-
+import{ UserModel} from './../Model/user.model.js';
 
 
 const userRegister = async (req, res) => {
-    res.json("it was working")
+    const { email , password , phone , firstName , lastName } = req.body
+
+    if ([email , password , phone , firstName , lastName ].some(e => e?.trim() == '' )) return res.status(404).json({message:"Please full fill the registration from"});
+
+    // check if user already exists
+    const user = await UserModel.findOne({email});
+    if(user) return res.status(409).json({message:"Email already exists try another email address"});
+
+    // create new user
+    const newUser = await UserModel.create( { email , password , phone , firstName , lastName })
+
+    res.status(200).json({message:"registation succesfull",newUser})
 }
 
-export { userRegister }
+const postProduct = async (req, res) => {
+    try {
+        
+        const {} = req.body;
+
+        
+    } catch (error) {
+        console.log(error.message , "Some error in postProduct");
+    }
+}
+
+export { userRegister , postProduct }
