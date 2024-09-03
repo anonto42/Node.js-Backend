cafa30502e80cf3cad759f522bb2bd60dd686a09
Sohidul Islam Ananto <anontom90@gmail.com>
2024-09-03 22:12:16 +0600

    get produc route added

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { userRegister , postProduct } from '../controller/user.controller.js';
+import { userRegister , postProduct ,  getProducts } from '../controller/user.controller.js';
 import { upload } from '../middleware/uploader.middleware.js';
 
 const createRoute = Router();
@@ -7,6 +7,8 @@ const createRoute = Router();
 createRoute.route("/register").post(userRegister);
 
 createRoute.route("/postProduct").post( upload.fields([{name:"image1",maxCount:1},{name:"image2",maxCount:1},{name:"image3",maxCount: 1}]) , postProduct);
+
+createRoute.route("/getProducts").get(getProducts);
 
 
 export default createRoute;
